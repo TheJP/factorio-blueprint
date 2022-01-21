442db09a851805f463900fd3b7ebfadcd0794fd6
TheJP <jp_55555@hotmail.com>
2022-01-21 14:33:03 +0100

    Refactored lib and bin create to make the separation useful

--- factorio-blueprint/src/lib.rs
+++ factorio-blueprint/src/lib.rs
@@ -1,46 +1,83 @@
 mod model;
 mod abstract_model;
 
-use std::io::{Read, self};
+use core::fmt;
+use std::{io::Read, error::Error};
 
+use base64::DecodeError;
 use flate2::read::ZlibDecoder;
 
 use crate::{model::BlueprintContainer, abstract_model::Blueprint};
 
-const _SAMPLE_BLUEPRINT: &str = "0eNrtWMuumzAQ/RcvK7jCBsJD6k9000UVIQJzE0tgI2OiRhH/3jG0uXkQCkRtIrWbRH6dGc85niNxJJuigUpxoUl8JDyToibxtyOp+VakhZnThwpITLiGklhEpKUZ5ZDxHJSdyXLDRaqlIq1FuMjhO4lpu7YICM01hx6tGxwS0ZQbULhhDMcilazxqBQmOsLZzLPIAf9d9uZjlJwryPp1ZhHMWCtZJBvYpXuO5/HQT9QE1/IOqTaz71zVOrm52J4r3eDMKad+h+2YG5l66NQUxzGDskpVl2RMPuMB2eiqmQG5VQCih60OmF0jdPKuZJlwgTgk1qqBto8q+it2iVPz0589KybPu0JmXGUN192QtdbFsne5TK+WV9en1xibmWAK8utQ7BoLOZ6SE25s27PAvzTAZmqAPUMDXy41QJ+sgQFabhRwjxZ3lF33DkvuKf0Sct6UNhSYlOKZXckChnhy3vwLpibeg40rObx7LzZL4/QW1xRbAN/uNrJRpl2F64FCeKdCpIrrXQkaazCta9GJiv0Afky0J4XVYHCSD/3aqBZZAcq3y4N8Wq7fR9oUHZXialr/8Bd6iPPfQy7r7Y+/vGj8AVFnjokEU03EnyaC1UIToX9TBF9f3kS8BSbyiPcHC2lznuj97PVo86fSFo4+yvAOS+ED3u/M8f5gvAO5d+8VjPcm9ze9ig6Yvztk/tFi82fRP2/+/hzzH6RogBGzb0kLmczHn+j8L9hCogUtZLphI1j32SA++8pgkT2oui99SL0gYoEX+V7gu237A6pardM=";
-const _SAMPLE_BLUEPRINT_DECIDER: &str = "0eNq1l91KwzAUx9/lgDfa6dLvljkQvFbwShAt/YgusKYlTcUy+gC+hRf6Yj6JSSeoo93kQK5Kkp7/+f1SaMgGsnVLa8G4hHgDLK94A/HdBhr2xNO1npNdTSEGJmkJFvC01KNUMLkqqWT5LK/KjPFUVgJ6Cxgv6AvEpLcOZhQ0ZwUV4wF2f28B5ZJJRrdEw6BLeFtmVKgO+3IsqKtGlVZcd1dxMxJa0KmnMz/1VJeCCZpv120LlLUU1TrJ6Cp9ZqpeFX2nJmqtGJIaPav3R6Z6s+a6rKxTMTSMYQF6ou5UQctl8iiqMmG8btWrUrS07/WO7DjYSAdiyGGJcHBwDnZoyOHz9QNh4SItIkMW5wgHD+ngG/sSbwgLH2kRGLN4R1gEB/6Ue0Tcf4r8BO+4NFSPkz9KVU2V0BAHxzCKHOKRbdPIJ+PIER7ZMY18No5M5njmuWnmowlmgmcmpplnE8w2mpmEppkXiwloBw8dmYZ+mGB28cy+aeaLq8sJag9PHZimXi4noH08tPFT5fb6ZoIafxYSzzT1FlpdNYarSfzrNmTBMxXNtmlI3CCyAzfy3MBz+v4LCKeZFQ==";
-const _SAMPLE_BLUEPRINT_XX: &str = "0eNqVkdtKxDAQht9lrrNCd1u6G/BCX8ILkZC2Ux1ok5BDsZS8u5NWRBBEr8Ic/m/mn2zQTQmdJxNBbkC9NQHk8waBXo2eSi6uDkECRZxBgNFziQbsaUB/6u3ckdHResgCyAz4DrLKLwLQRIqEB20PVmXS3KHnht84ApwNLLWmTGfcqboKWPm91HcNTxnIY3/UzwJ44+jtpDp80wuxnkWfVMW1YSeFkh3Jh6h+GFvIx8SZr52OjtNDcRSwMP4ueiwiNuO0381IuOcem6JL/xj9dFDcyg6SiWr0dlZkmAFy1FPAnMuJ9y+R335QwII+HJe5VnV7O7f1ranb5pLzB5+lp6U=";
+#[derive(Debug, PartialEq)]
+pub enum BlueprintError {
+    InvalidVersion,
+    Base64Decode(DecodeError),
+    ZlibInflate,
+    JsonDecode,
+    JsonDeserialize,
+    JsonEncode,
+    JsonSerialize,
+}
 
-fn main() -> io::Result<()> {
+impl fmt::Display for BlueprintError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Self::InvalidVersion => write!(f, "blueprint has invalid version"),
+            Self::Base64Decode(ref cause) => write!(f, "base64 decoding of blueprint failed: {}", cause),
+            Self::ZlibInflate => write!(f, "zlib inflation of blueprint failed"),
+            Self::JsonDecode => write!(f, "json decode of blueprint failed"),
+            Self::JsonDeserialize => write!(f, "json deserialize of blueprint failed"),
+            Self::JsonEncode => write!(f, "json encode of blueprint failed"),
+            Self::JsonSerialize => write!(f, "json serialize of blueprint failed"),
+        }
+    }
+}
+
+impl Error for BlueprintError {}
+
+pub type Result<T> = core::result::Result<T, BlueprintError>;
+
+pub fn blueprint_string_to_json(blueprint: &str) -> Result<serde_json::Value> {
     // Check version
-    assert_eq!('0', _SAMPLE_BLUEPRINT.chars().nth(0).unwrap());
+    match blueprint.chars().nth(0) {
+        Some('0') => {}
+        _ => return Err(BlueprintError::InvalidVersion)
+    }
 
     // Base64 Decode
-    let decoded = base64::decode(&_SAMPLE_BLUEPRINT[1..]).unwrap();
+    let decoded = base64::decode(&blueprint[1..])
+        .map_err(|cause|BlueprintError::Base64Decode(cause))?;
 
     // Zlib Inflate
     let mut inflator = ZlibDecoder::new(&decoded[..]);
     let mut json = String::new();
-    inflator.read_to_string(&mut json)?;
+    inflator.read_to_string(&mut json)
+        .or(Err(BlueprintError::ZlibInflate))?;
 
     // Json Decode
-    let json: serde_json::Value = serde_json::from_str(&json)?;
-    let pretty = serde_json::to_string_pretty(&json)?;
+    serde_json::from_str(&json)
+        .or(Err(BlueprintError::JsonDecode))
+}
+
+pub fn blueprint_string_to_raw_model(blueprint: &str) -> Result<BlueprintContainer> {
+    let json = blueprint_string_to_json(blueprint)?;
+    let pretty = serde_json::to_string_pretty(&json)
+        .or(Err(BlueprintError::JsonEncode))?;
 
     // Json Deserialize
     // Using pretty instead of the raw json string makes errors use more usefull line numbers.
-    let json: BlueprintContainer = serde_json::from_str(&pretty)?;
-    // println!("{:#?}", json);
+    serde_json::from_str(&pretty)
+        .or(Err(BlueprintError::JsonDeserialize))
+}
 
-    // let pretty = serde_json::to_string_pretty(&json)?;
-    // println!("{}", pretty);
+pub fn blueprint_string_to_model(blueprint: &str) -> Result<Blueprint> {
+    let raw_model = blueprint_string_to_raw_model(blueprint)?;
+    Ok(Blueprint::from(raw_model))
+}
 
-    let blueprint = Blueprint::from(json);
-    // println!("{:#?}", blueprint);
-
-    let reconstructed: BlueprintContainer = blueprint.into();
-    let pretty = serde_json::to_string_pretty(&reconstructed)?;
-    println!("{}", pretty);
-
-    Ok(())
+pub fn model_to_pretty_json(model: Blueprint) -> Result<String> {
+    let raw_model: BlueprintContainer = model.into();
+    serde_json::to_string_pretty(&raw_model)
+        .or(Err(BlueprintError::JsonEncode))
 }
